0f5d8d23e23f8d214f94ce3da0a534cb9df7e2c3
Vincent <2w121q@gmail.com>
2024-03-28 14:41:02 +0800

    Add 3-day forecast

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './style.css';
-// 1cadc94424cb419a892105402242703
+
 async function getWeather(place) {    
-    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=1cadc94424cb419a892105402242703&q=${place}&days=3`, {mode: 'cors'})
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=1cadc94424cb419a892105402242703&q=${place}&days=4`, {mode: 'cors'})
     let data = await response.json();
-    console.log(data);
     let processedData = processData(data);
-    console.log(processedData);
     displayData(processedData);
     
     function toggleTemp() {
@@ -17,16 +15,18 @@ async function getWeather(place) {
         else temp.textContent = processedData.tempFah;
     }
     let tog = document.querySelector('#tog');
-    tog.onclick = toggleTemp;
-    
+    tog.onclick = toggleTemp;    
 }
 
 function processData(data) {
     let tempCel = data.current.temp_c;
     let tempFah = data.current.temp_f;
     let condition = data.current.condition.text.toLowerCase();
+    let day1 = data.forecast.forecastday['1'].day.condition.text.toLowerCase();
+    let day2 = data.forecast.forecastday['2'].day.condition.text.toLowerCase();
+    let day3 = data.forecast.forecastday['3'].day.condition.text.toLowerCase();
     let location = data.location.name;
-    return { tempCel, tempFah, condition, location };
+    return { tempCel, tempFah, condition, day1, day2, day3, location };
 }
 
 let unit = 'Celsius';
@@ -35,6 +35,9 @@ function displayData(data) {
     let con = document.querySelector('#con');
     let loc = document.querySelector('#loc');
     let temp = document.querySelector('#temp');
+    let day1 = document.querySelector('#day1');
+    let day2 = document.querySelector('#day2');
+    let day3 = document.querySelector('#day3');
     
     if (data.condition.includes('rain')) {
         body.style.backgroundColor = '#C5E2F7';
@@ -48,21 +51,31 @@ function displayData(data) {
         body.style.backgroundColor = 'orange';
         con.style.color = 'orange';
     }
-    else body.style.backgroundColor = '#000';
+    else {
+        body.style.backgroundColor = '#000';
+        con.style.color = 'white';
+    }
     
     con.textContent = data.condition;
     loc.textContent = data.location;
-
     if (unit === 'Celsius') temp.textContent = data.tempCel;
-    else temp.textContent = data.tempFah;    
-}
+    else temp.textContent = data.tempFah;
 
+    day1.textContent = data.day1;
+    day2.textContent = data.day2;
+    day3.textContent = data.day3;
+}
 
 let go = document.querySelector('#go');
 go.addEventListener('click', () => {
     let tog = document.querySelector('#tog');
-    tog.removeAttribute('disabled');
+    let error = document.querySelector('#error');
     let input = document.querySelector('input');
-    getWeather(input.value);
+    if (input.value) {
+        error.textContent = '';
+        tog.removeAttribute('disabled');
+        getWeather(input.value);
+    } else {
+        error.textContent = 'Enter a value first';
+    }
 });
-
